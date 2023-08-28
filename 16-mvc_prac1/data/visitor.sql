d4b0e01e53546bc2318bf8332e979bea2d661707
정채림 <119480528+ellin45@users.noreply.github.com>
2023-08-28 15:53:09 +0900

    "study : mvc with mysql로 read all 구현"

---
+++ 16-mvc_prac1/data/visitor.sql
@@ -0,0 +1,46 @@
+create database data default character set utf8 collate utf8_general_ci;
+
+use data;
+
+create table visitor(
+id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+name varchar(10) not null,
+comment mediumtext null
+);
+
+desc visitor;
+
+select * from visitor;
+
+insert into visitor(id, name, comment) values('1', '홍길동', '내가 왔다');
+insert into visitor(id, name, comment) values('2', '이찬혁', '으라차차');
+
+show databases;
+
+drop database data;
+
+create database data1 character set utf8mb4 collate utf8mb4_unicode_ci;
+
+create table visitor(
+id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+name varchar(10) not null,
+comment mediumtext null
+);
+
+desc visitor;
+
+create user 'user'@'%' identified by '1234';
+
+drop user 'user'@'%';
+create user 'user'@'%' identified with mysql_native_password by '1234';
+
+
+grant all privileges on *.* to 'user'@'%' with grant option;
+
+flush privileges;
+
+
+show databases;
+
+select * from mysql.user;
+
