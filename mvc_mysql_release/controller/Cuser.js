055f09153d488eeaa66074e38f66fa0965f7f4ef
정채림 <chaelimj45@gmail.com>
2023-08-31 01:16:11 +0900

    "study : sequelize 과제 진행중"

--- mvc_mysql_release/controller/Cuser.js
+++ mvc_mysql_release/controller/Cuser.js
@@ -1,47 +1,78 @@
 // TODO: 컨트롤러 코드
-const User = require('../model/User');
+const { User } = require('../model/User');
 
 exports.main = (req, res) => {
   res.render('index');
 };
 
-exports.signup = (req, res) => {
+exports.signup = async(req, res) => {
   res.render('signup');
 };
 
-exports.postSignup = (req, res) => {
-  User.postSignup(req.body, (result) => {
+exports.postSignup = async(req, res) => {
+
+  const {userid, pw, name} = req.body;
+  const result = await User.create({
+    userid,
+    pw,
+    name
+  });
     res.send(result);
-  });
 };
 
-exports.signin = (req, res) => {
-  res.render('signin');
-};
 
-exports.postSignin = (req, res) => {
-  User.signin(req.body, (result) => {
+exports.postSignin = async(req, res) => {
+  const { userid, pw } = req.body;
+  
+  try {
+    const result = await User.findOne({
+      where: {userid, pw},
+    });
     res.send(result);
-  });
+  }catch(err){
+    res.send(false);
+  }
 };
 
-exports.profile = (req, res) => {
-  const userid = req.body.userid;
-
-  User.profile(userid, (result) => {
-    res.render('profile', { userInfo: result });
+exports.profile = async(req, res) => {
+  const {userid} = req.body;
+  const result = await User.findOne({
+    where: {id: userid},
   });
+  
+  res.send('profile', { user: {
+    user: {
+      id:result.id,
+      userid: result.userid,
+      pw: result.pw,
+      name: result.name,
+    }
+  } });
+ 
 };
 
-exports.editProfile = (req, res) => {
-  User.editProfile(req.body, (result) => {
-    res.send(result);
-  });
+exports.editProfile = async(req, res) => {
+  const { id, userid, pw, name } = req.body;
+  const result = await User.update(
+    {
+      userid,
+      pw,
+      name,
+    },
+    {
+      where: {
+        id,
+      },
+    }
+  );
+  res.send(true);
 };
 
-exports.deleteUser = (req, res) => {
-  User.deleteUser(req.body, (result) => {
-    res.send(result);
-    console.log(result);
+exports.deleteUser = async(req, res) => {
+
+  const { id } = req.body;
+  const result = await User.destroy({
+    where : {id : id},
   });
+  res.send(true);
 };
