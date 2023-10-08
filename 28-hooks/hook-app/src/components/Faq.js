d954736666f6678a3e01a140b84452b72af0c005
정채림 <chaelimj45@gmail.com>
2023-10-08 23:28:11 +0900

    "fix: useToggle 수정"

--- 28-hooks/hook-app/src/components/Faq.js
+++ 28-hooks/hook-app/src/components/Faq.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import useToggle from '../hooks/useToggle';
+import useToggle from './hooks/useToggle';
 
 export default function Faq() {
   const [isFaqOpen, setIsFaqOpen] = useToggle();
