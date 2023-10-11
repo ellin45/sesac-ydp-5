cb6e36e0a6caff6e4dc648cc8f71e6b5549bb10a
정채림 <119480528+ellin45@users.noreply.github.com>
2023-10-11 14:18:56 +0900

    "study : css module"

---
+++ 29-style/style-app/src/App.js
@@ -0,0 +1,13 @@
+import '../src/styles/App.css'
+
+function App() {
+  return (
+    <div className="App">
+    <h1>React Styling</h1>
+    <h2>Css module</h2>
+    <cssModuleComponents/>
+    </div>
+  );
+}
+
+export default App;
