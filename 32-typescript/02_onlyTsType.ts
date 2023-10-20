c1fc589238d1c15b23295bfcc3cfcdd58efef764
정채림 <119480528+ellin45@users.noreply.github.com>
2023-10-20 15:49:54 +0900

    "study : typescript 문법"

--- 32-typescript/02_onlyTsType.ts
+++ 32-typescript/02_onlyTsType.ts
@@ -52,3 +52,133 @@ val2 = '바이';
 type olympic = readonly [object, boolean];
 let game: olympic = [{name: "쇼트트랙", type: "혼성 계주"}, true];
 
+///////////////////////////////////////////////////
+//Type vs interface
+
+//1. interface
+interface Student {
+  name: string;
+  isPassed: boolean;
+  // addr: string;
+}
+
+const student1: Student = {
+  name: 'jinheyoung',
+  isPassed: true,
+  // addr: 'seoul',
+}
+const student2: object = {
+  name: 'jinheyoung',
+  isPassed: true,
+  // addr: 'seoul',
+}
+
+// interface 상속
+//A+ A, B, C, D, F
+// type Score = 'A+' | 'B' | 'C'|'D'|'F';
+
+enum Score {
+  Aplus = 'A+',
+  A = 'A',
+  B = 'B',
+  C = 'C',
+  D = 'D',
+  F = 'F',
+}
+interface BaseballClub extends Student{
+  position:string;
+  height: number;
+  backNumber?: number; // ?: 있어도 되고 없어도 됨.
+  // [grade: number]: string;
+  [grade: number]: Score;
+}
+
+const otani: BaseballClub = {
+  name: 'otani',
+  isPassed: true,
+  position: 'pitcher',
+  height: 193,
+  1: 'A+' // 학년: 점수
+  // 2: 'NP',
+};
+console.log(otani);
+
+otani.position = '투수';
+otani['height'] = 200;
+// otani.backNumber = 16; //error: backNumber readonly
+console.log(otani);
+
+
+//type vs enum
+type Bp1 = 500 | 700 | 1000;
+enum Bp2{
+  SM = 500,
+  MD = 700,
+  LG = 1000,
+}
+
+const width1: Bp1 = 500;
+const width2: Bp2 = Bp2.SM;
+
+//교차 타입
+interface Toy {
+  name: string;
+  start(): void;
+}
+
+interface Car{
+  name: string;
+  color: string;
+  price: number;
+}
+
+type ToyCar = Toy & Car;
+const toyCar: Toy & Car = {
+  name: 'tayo',
+  start() {
+    console.log('출발!!!');
+  },
+  color: 'blue',
+  price: 5000,
+};
+
+//2. type
+type Gender = 'F' | 'M';
+type Person = {
+  name: string,
+  age?: number;
+  like?: string[];
+  gender: string;
+}
+
+let daniel: Person = {
+  name: 'daniel',
+  age: 20,
+  gender: 'F', // Gender 타입에 선언된 값만 넣을수 있음
+  like: ['minji', 'hani'],
+}
+type category = 'web' | 'mobile';
+interface Game {
+ title: string;
+ price: number;
+ desc?: string;
+ category: string;
+ platform: string;
+}
+
+let heroGame_A:Game = {
+  title: '동물의 숲',
+  price : 450000,
+  desc: '모두 모여봐요 동물의 숲',
+  category: '멀티 닌텐도 게임 ',
+  platform : 'mobile'
+}
+
+let heroGame_B:Game = {
+  title: '메이플 스토리',
+  price : 5000,
+  desc: '캐릭터 키우는 게임',
+  category: 'MMORPG',
+  platform : 'web'
+}
+
